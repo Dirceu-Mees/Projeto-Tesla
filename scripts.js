53cd69b004a0af80e82c61f0b75618ba2d2c000b
Dirceu Mees <dirceumees@gmail.com>
2024-07-21 18:02:27 -0300

    inicizndo projeto

---
+++ scripts.js
@@ -0,0 +1,30 @@
+//alert("olá mundo")
+
+//let bobEsponja ("oi mundo")
+//console.log(bobEsponja)
+
+//function abacate(){
+ //   console.log("")
+
+//abacate
+
+//site JOTFORM, para baixar cadastros grátis,
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left = "50%"
+
+}
+
+function desaparecerFormulario() {
+    formulario.style.left = "-50%"
+}
+
+
+//function desaparecerFormulario(){
+ //   desaparecerFormulario.style.left = "-50%"
+//}
+//<script type="text/javascript" src="https://form.jotform.com/jsform/241986262945065"></script>
+
+//
